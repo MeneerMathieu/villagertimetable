3c176bedc1e9bc4aba3cb76534999a727f1d3b7c
MeneerMathieu <61655741+MeneerMathieu@users.noreply.github.com>
2025-09-15 22:23:36 +0200

    Fix crash with double renderBackground call

--- src/main/java/winniethedampoeh/villagertimetable/config/VillagerTimetableConfigScreen.java
+++ src/main/java/winniethedampoeh/villagertimetable/config/VillagerTimetableConfigScreen.java
@@ -52,7 +52,6 @@ public class VillagerTimetableConfigScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        renderBackground(context, mouseX, mouseY, delta);
         super.render(context, mouseX, mouseY, delta);
         context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 5, 0xffffff);
     }
